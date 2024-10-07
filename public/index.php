fe7463b836603e7dd6098d31cdd7e4c2746ec1e3
Liam <37994@ma-web.nl>
2024-10-07 12:07:21 +0200

    firstcomit

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+// require_once '../php/database.php';
+// $connection = database_connect();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>bo opdracht</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="build/styles.css">
+    <script src="js/main.js" defer></script>
+</head>
+
+<body>
+    <img id="background-img" src="images/background.webp" alt="background image">
+    <div id="main__wrapper">
+        <header class="header">
+
+        </header>
+        <main class="main">
+
+        </main>
+    </div>
+    <footer class="footer">
+
+    </footer>
+</body>
+
+</html>
